bb250ab8608ba7431eddf1f65a02a0cad53108c3
zoeyyhe <150974745+zoeyyhe@users.noreply.github.com>
2024-03-06 14:19:53 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,16 +1,70 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+#!/bin/bash
 
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
+GRADING_AREA="grading-area"
+
+# Clean up previous runs
 rm -rf student-submission
-rm -rf grading-area
+rm -rf $GRADING_AREA
 
-mkdir grading-area
+# Create directories
+mkdir $GRADING_AREA
 
-git clone $1 student-submission
+# Clone the student's submission
+git clone $1 student-submission > /dev/null 2>&1
 echo 'Finished cloning'
 
+# Check if the required file exists
+if [ ! -f student-submission/ListExamples.java ]; then
+    echo "Error: ListExamples.java not found in the student's submission."
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+# Move necessary files to grading-area
+cp student-submission/ListExamples.java TestListExamples.java $GRADING_AREA
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cp -r lib $GRADING_AREA
+
+# Change to the grading-area directory
+cd $GRADING_AREA
+
+# Compile the code
+javac -cp $CPATH *.java
+
+# Check if compilation was successful
+if [ $? -eq 0 ]
+then 
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+fi
+
+# Disable 'set -e' temporarily for running tests
+set +e
+
+# Run the tests and report the grade based on JUnit output
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test_results.txt
+
+# Enable 'set -e' again
+set -e
+
+# Detect failed tests
+if grep -q "FAILURES!!!" test_results.txt; then
+    echo "Some tests failed. Please review the test results."
+    # cat test_results.txt
+else
+    echo "All tests passed."
+    echo "Total Grades: 100"
+fi
+
+# Count the total number of tests and the number of passed tests
+total_tests=$(grep -oP "(?<=Tests run: )\d+" test_results.txt)
+failed_test=$(grep -oP "(?<=Failures: )\d+" test_results.txt)
+total_tests=$((total_tests))
+failed_test=$((failed_test))
+passed=$((total_tests-failed_test))
+grades=$((passed/total_tests*100))
+echo "Total Grades: $grades"
+
+# Clean up
+rm test_results.txt
